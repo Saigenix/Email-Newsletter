63fb2518fea17d537774bfc9ec8a8ce7e70aa627
saigenix <sainathmahindrakar@gmail.com>
2023-09-03 01:34:23 +0530

    update

---
+++ emails.js
@@ -0,0 +1,24 @@
+// setup encryption for this
+
+
+exports.EmailList = [
+  {
+    name: "sai",
+    email: "sainathkumar805@gmail.com",
+  },
+  {
+    name: "rahul",
+    email: "rahulchafle1004@gmail.com",
+  },
+  {
+    name: "sainath",
+    email: "2021bcs133@sggs.ac.in",
+  },
+  {
+    name:"shivkiran",
+    email:"2021bcs021@sggs.ac.in"
+
+  }
+];
+
+
